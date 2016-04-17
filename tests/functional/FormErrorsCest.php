25765fc14cf7fa411bb3e47b42afcb90bddaa6b0
tung.chung <tung.chung@seldatinc.com>
2016-04-17 15:02:14 +0700

    make test failure

--- tests/functional/FormErrorsCest.php
+++ tests/functional/FormErrorsCest.php
@@ -20,29 +20,29 @@ class FormErrorsCest
         $I->dontSeeFormErrors();
     }
 
-//    public function testSeeFormErrorMessage(FunctionalTester $I)
-//    {
-//        $I->seeFormErrorMessage('name');
-//        $I->seeFormErrorMessage('name', 'required');
-//        $I->seeFormErrorMessage('name', 'The name field is required.');
-//    }
+    public function testSeeFormErrorMessage(FunctionalTester $I)
+    {
+        $I->seeFormErrorMessage('name');
+        $I->seeFormErrorMessage('name', 'required');
+        $I->seeFormErrorMessage('name', 'The name field is required.');
+    }
 
-//    public function testSeeFormErrorMessages(FunctionalTester $I)
-//    {
-//        $I->seeFormErrorMessages(array(
-//            'name' => null,
-//            'email' => null,
-//        ));
-//
-//        $I->seeFormErrorMessages(array(
-//            'name' => 'required',
-//            'email' => 'required',
-//        ));
-//
-//        $I->seeFormErrorMessages(array(
-//            'name' => 'The name field is required.',
-//            'email' => 'The email field is required.'
-//        ));
-//    }
+    public function testSeeFormErrorMessages(FunctionalTester $I)
+    {
+        $I->seeFormErrorMessages(array(
+            'name' => null,
+            'email' => null,
+        ));
+
+        $I->seeFormErrorMessages(array(
+            'name' => 'required',
+            'email' => 'required',
+        ));
+
+        $I->seeFormErrorMessages(array(
+            'name' => 'The name field is required.',
+            'email' => 'The email field is required.'
+        ));
+    }
 
 }
