e87f4bd630a720ee23c446cf16647272befa2f17
Tung Chung <kokocaca113@gmail.com>
2016-04-18 08:07:50 +0700

    hot fix 1

--- tests/functional/SayHelloCest.php
+++ tests/functional/SayHelloCest.php
@@ -8,4 +8,10 @@ class SayHelloCest
         $I->amOnPage('/say-hello');
         $I->see('say hello for ticket 1');
     }
+
+    public function sayHelloForHotFix1(FunctionalTester $I)
+    {
+        $I->amOnPage('/say-hello-hot-fix');
+        $I->see('say hello for HF ticket 1');
+    }
 }
