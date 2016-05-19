5364abc34e73e49187058149596be9cd622bb47f
tungchung <tung.chung@seldatinc.com>
2016-05-19 12:02:19 +0700

    remove codeception test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+class ExampleTest extends TestCase {
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $response = $this->call('GET', '/');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
--- tests/_bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This should be added to Laravel5 module itself
-ini_set('xdebug.max_nesting_level', 500);
